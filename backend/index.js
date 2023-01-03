665026c8e424d4afec77b5e92de4ae84b15a90d2
Eorelynne <89979161+Eorelynne@users.noreply.github.com>
2023-01-03 13:33:35 +0100

    Clean up template, Proxy setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import { createRequire } from "module";
+// Only necessary if you need  __filename or __dirname
+import { fileURLToPath } from "url";
+
+// Note: import.meta.url is different for every file
+const { url } = import.meta;
+// Make the require function available
+const require = createRequire(url);
+// Set __filename and __dirname constants
+const __filename = fileURLToPath(url);
+const __dirname = fileURLToPath(new URL(".", url));
+
+const express = require("express");
+const app = express();
+const port = 4000;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
+
+app.get("/api/", (req, res) => {
+  res.json({ name: "Holly" });
+});
